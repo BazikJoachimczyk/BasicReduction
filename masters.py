2bf5fdcd912d28efae2f65bd3f74371d4d20206c
Barbara Joachimczyk <312255@stud.umk.pl>
2024-09-11 12:40:28 +0200

    Scaling darks added to masters.py

--- masters.py
+++ masters.py
@@ -1,5 +1,7 @@
 ### Klasa obiektów: Masters. Przechowuje obiekty typu Frame(), które są wyznaczonymi masterramkami w celu wykorzystania ich we właściwej redukcji ramek naukowych.
 
+from frame import Frame
+
 class Masters():
     def __init__(self):
         self.bias = []
@@ -10,7 +12,21 @@ class Masters():
         for dark in self.dark:
             if (dark.exp == exp and dark.bin == bin and dark.subx == subx and dark.suby == suby):
                 return dark
-        
+        if dark == None:
+            for dark in self.dark:
+                if (dark.bin == bin and dark.subx == subx and dark.suby == suby):
+                    scaling_factor = dark.exp / exp
+                    ScaledDark = Frame()
+                    ScaledDark.data = dark.data * scaling_factor
+                    ScaledDark.exp = exp
+                    ScaledDark.bin = bin 
+                    ScaledDark.subx = subx 
+                    ScaledDark.suby = suby
+                    self.dark.append(ScaledDark)
+                    print(f'Scaling dark from Exp: {dark.exp}, Bin: {dark.bin}, Subx: {dark.subx}, Suby: {dark.suby}')
+                    return dark
+
+
         print(f'[ERROR] Dark not found. Failed to find one with params: Exp {exp}, Bin {bin}, Subx {subx}, Suby {suby}')  
 
     def GetFlatByFilter(self, filter, bin, subx, suby):
